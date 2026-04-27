586cbc499a3b70441da7a70a7385c38a33b10c19
zoiska <zoisk64@gmail.com>
2026-04-27 18:23:18 +0200

    fix directionalLighting pos eval as string to array, scannerView exp def f ScannerView()

--- src/pages/FrameView.jsx
+++ src/pages/FrameView.jsx
@@ -1,15 +1,15 @@
 import { Canvas } from "@react-three/fiber";
-import BoxFrame from "./components/BoxFrame";
+import BoxFrame from "../components/BoxFrame";
 
 export default function App() {
   return (
     <>
-    <h1>Test</h1>
-    <Canvas>
-      <directionalLight position={0, 0, 2} intensity={1.5}/>
-      <ambientLight intensity={1}/>
-      <BoxFrame rotation="0.7854" color="white"/>
-    </Canvas>
+      <h1>Test</h1>
+      <Canvas>
+        <directionalLight position={[2, 2, 2]} intensity={1.5} />
+        <ambientLight intensity={1} />
+        <BoxFrame rotation="0.7854" color="white" />
+      </Canvas>
     </>
   );
 }
---
+++ src/pages/ScannerView.jsx
@@ -0,0 +1,7 @@
+export default function ScannerView() {
+  return (
+    <>
+      <h1>Scan QR-Code</h1>
+    </>
+  );
+}
