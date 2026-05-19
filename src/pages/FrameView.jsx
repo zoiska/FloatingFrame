e639092d34d83166b0ce0d39b2c977daa5a92a19
zoiska <zoisk64@gmail.com>
2026-05-19 02:45:58 +0200

    testing

--- src/pages/FrameView.jsx
+++ src/pages/FrameView.jsx
@@ -1,15 +1,24 @@
 import { Canvas } from "@react-three/fiber";
 import BoxFrame from "../components/BoxFrame";
+import NotebookModel from "../components/NotebookModel";
 
 export default function App() {
   return (
-    <>
-      <h1>Test</h1>
-      <Canvas>
-        <directionalLight position={[2, 2, 2]} intensity={1.5} />
-        <ambientLight intensity={1} />
-        <BoxFrame rotation="0.7854" color="white" />
-      </Canvas>
-    </>
+    <div className="mainContainer h-dvh w-full">
+      <div className="switchContainer h-1/10 w-full border-pink-600 border-2">
+        Switch visuals placeholder
+      </div>
+      <div className="frameContainer h-4/10">
+        <Canvas className="canvas border-green-600 border-2">
+          <directionalLight position={[2, 2, 2]} intensity={1.5} />
+          <ambientLight intensity={1} />
+          <BoxFrame rotation="0.7854" color="white" />
+          <NotebookModel position={[0, -0.6, 2]} rotation={[0, -0.7, 0]} scale={0.3} />
+        </Canvas>
+      </div>
+      <div className="detailsContainer h-5/10 w-full border-red-600 border-2">
+        {/** tbd: animation, dynamic frameheight, details height, close details */}
+      </div>
+    </div>
   );
 }
