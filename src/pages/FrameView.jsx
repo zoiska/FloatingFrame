b3427c1a664fbd5e26fb40e65bf3bc4ebe1eb779
zoiska <zoisk64@gmail.com>
2026-06-02 02:28:02 +0200

    patchpanel import, style changes

--- src/pages/FrameView.jsx
+++ src/pages/FrameView.jsx
@@ -3,39 +3,41 @@ import { useContext } from "react";
 import BoxFrame from "../components/BoxFrame";
 import NotebookModel from "../components/NotebookModel";
 import { ScannedCodesContext } from "../contexts/ScannedCodesContext";
+import { Patchpanel } from "../react_assets/Patchpanel";
 
 export default function App() {
   const { scannedCodes } = useContext(ScannedCodesContext);
 
   return (
     <div className="mainContainer h-dvh w-full">
-      <div className="switchContainer h-1/10 w-full border-pink-600 border-2">
-        Switch visuals placeholder
+      <div className="switchContainer h-1/10 w-full justify-center">
+        <Patchpanel />
       </div>
       <div className="frameContainer h-4/10">
-        <Canvas className="canvas border-green-600 border-2">
+        <Canvas className="canvas">
           <directionalLight position={[2, 2, 2]} intensity={1.5} />
           <ambientLight intensity={1} />
           <BoxFrame rotation="0.7854" color="white" />
           <NotebookModel position={[0, -0.6, 2]} rotation={[0, -0.7, 0]} scale={0.3} />
         </Canvas>
       </div>
-      <div className="detailsContainer h-5/10 w-full border-red-600 border-2 wrap-anywhere overflow-auto">
-        <div className="whitespace-break-spaces p-2">
-          {scannedCodes.map((code, index) => (
-            <pre key={index}>
-              {Object.entries(code)
-                .map(([key, value]) => {
-                  let remUnderscores = key.replaceAll("_", " ");
-                  let newLabel = remUnderscores.charAt(0).toUpperCase() + remUnderscores.slice(1);
-                  return `${newLabel}: ${value}`;
-                })
-                .join("\n")}
-            </pre>
-          ))}
-        </div>
-        {/** tbd: animation, dynamic frameheight, details height, close details */}
+      <div className="detailsContainer h-5/10 w-full wrap-anywhere overflow-auto">
+        {scannedCodes.map((code, index) => (
+          <pre key={index} className="whitespace-break-spaces p-2">
+            {Object.entries(code).map(([key, value]) => {
+              let remUnderscores = key.replaceAll("_", " ");
+              let newLabel = remUnderscores.charAt(0).toUpperCase() + remUnderscores.slice(1);
+              return (
+                <div>
+                  <span className="font-bold text-brand-blue">{`${newLabel}: `}</span>
+                  <span>{`${value}\n`}</span>
+                </div>
+              );
+            })}
+          </pre>
+        ))}
       </div>
+      {/** tbd: animation, dynamic frameheight, details height, close details */}
     </div>
   );
 }
