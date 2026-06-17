061ffd85ad75d206d3d59ebded8d7011b26c07b5
zoiska <zoisk64@gmail.com>
2026-06-18 01:35:59 +0200

    add qrCodeService get fetch

---
+++ src/services/qrCodeService.js
@@ -0,0 +1,19 @@
+// ping backend to get data from a the num provided by scanning a qr code
+const API_URL = import.meta.env.VITE_API_URL;
+
+export async function qrCodeService(code) {
+  try {
+    const res = await fetch(`${API_URL}/api/qr/${code}`);
+    if (!res.ok) {
+      //TODO: user über error berichten undso
+      console.log(res.status);
+      const error = await res.json();
+      console.log("An error occured: ", error);
+    } else {
+      const data = await res.json();
+      console.log(data);
+    }
+  } catch (err) {
+    console.log("An error occured: ", err);
+  }
+}
