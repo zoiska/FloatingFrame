68dceb731e506bd7b1c09e1768215edd546a7861
zoiska <zoisk64@gmail.com>
2026-06-27 23:09:07 +0200

    minor service changes

--- src/services/assetService.js
+++ src/services/assetService.js
@@ -5,17 +5,15 @@ const API_URL = import.meta.env.VITE_API_URL;
 export async function assetService(setAssetResponseArray) {
   try {
     const res = await fetch(`${API_URL}/api/assets`);
-
     if (!res.ok) {
       const error = await res.json();
       console.error("API Error:", res.status, error);
       return;
+    } else {
+      const data = await res.json();
+      console.log("Successfully fetched asssets from server:", data);
+      setAssetResponseArray(data);
     }
-
-    const data = await res.json();
-    console.log("PASSED:", data);
-
-    setAssetResponseArray(data);
   } catch (err) {
     console.error("Network error:", err);
   }
